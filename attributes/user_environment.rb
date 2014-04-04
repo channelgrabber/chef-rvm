78b79dc2fcf21befd237e1d9e798f67673c47bfb
Adam <adamb@channelgrabber.com>
2014-04-04 11:25:52 +0100

    CGIV-1937 convert default path attribute hash to array

--- attributes/user_environment.rb
+++ attributes/user_environment.rb
@@ -1,7 +1,7 @@
-default['rvm']['user_env']['paths'] = {
+default['rvm']['user_env']['paths'] = [
   "/var/www/.rvm/gems/#{node['rvm']['user_default_ruby']}/bin",
   "/var/www/.rvm/gems/#{node['rvm']['user_default_ruby']}@global/bin",
   "/var/www/.rvm/rubies/#{node['rvm']['user_default_ruby']}/bin"
-}
+]
 
 default['rvm']['user_env']['profile_filename'] = ".profile"
