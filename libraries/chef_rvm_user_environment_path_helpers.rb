a8b1b70d37a6596ca3508fc3466f65dbff116956
Adam <adamb@channelgrabber.com>
2014-04-04 11:20:15 +0100

    CGIV-1937 moved writing to .profile into ruby block so it's successfully executed

--- libraries/chef_rvm_user_environment_path_helpers.rb
+++ libraries/chef_rvm_user_environment_path_helpers.rb
@@ -3,17 +3,25 @@ class Chef
     module UserEnvironmentPathHelpers
 
       def configure_ruby_env_paths(user_dir)
-        user_profile = File.join(user_dir, node['rvm']['user_env']['profile_filename'])
+        user_profile_filename = node['rvm']['user_env']['profile_filename']
+        user_profile = File.join(user_dir, user_profile_filename)
         file user_profile do
             action :touch
         end
 
-        node['rvm']['user_env']['paths'].each do |path|
-          bin = File.join(user_dir, path)
-          fe = Chef::Util::FileEdit.new(user_profile)
-          fe.insert_line_if_no_match(/#{bin}/, "export PATH=#{bin}:$PATH")
+        ruby_block "Add rvm paths to user's #{user_profile_filename}" do
+          block do
+            fe = Chef::Util::FileEdit.new(user_profile)
+
+            node['rvm']['user_env']['paths'].each do |path|
+              bin = File.join(user_dir, path)
+              fe.insert_line_if_no_match(/#{bin}/, "export PATH=#{bin}:$PATH")
+            end
+
+            fe.write_file
+          end
         end
-        fe.write_file
+
       end
 
     end
