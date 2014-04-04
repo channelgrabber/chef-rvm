34c17cb11036c165aebec1f1e558a6ac062b0158
Adam <adamb@channelgrabber.com>
2014-04-04 11:04:48 +0100

    CGIV-1937 Added default .profile filename to attributes

--- attributes/user_environment.rb
+++ attributes/user_environment.rb
@@ -3,3 +3,5 @@ default['rvm']['user_env']['paths'] = {
   "/var/www/.rvm/gems/#{node['rvm']['user_default_ruby']}@global/bin",
   "/var/www/.rvm/rubies/#{node['rvm']['user_default_ruby']}/bin"
 }
+
+default['rvm']['user_env']['profile_filename'] = ".profile"
