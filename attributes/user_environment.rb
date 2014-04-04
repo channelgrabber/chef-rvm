b3d472d146d8390e41a27e7afe88d310d40dfbc4
Adam <adamb@channelgrabber.com>
2014-04-04 10:53:39 +0100

    CGIV-1937 added default user env path attribute

---
+++ attributes/user_environment.rb
@@ -0,0 +1,5 @@
+default['rvm']['user_env']['paths'] = {
+  "/var/www/.rvm/gems/#{node['rvm']['user_default_ruby']}/bin",
+  "/var/www/.rvm/gems/#{node['rvm']['user_default_ruby']}@global/bin",
+  "/var/www/.rvm/rubies/#{node['rvm']['user_default_ruby']}/bin"
+}
