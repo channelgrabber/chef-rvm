42cb736e55ce02cafc1e0b3dfa3d6748ec2397a7
Adam <adamb@channelgrabber.com>
2014-04-04 11:05:11 +0100

    CGIV-1937 implemented method to configure ruby env path for a user

--- libraries/chef_rvm_user_environment_path_helpers.rb
+++ libraries/chef_rvm_user_environment_path_helpers.rb
@@ -1,7 +1,21 @@
 class Chef
   module RVM
     module UserEnvironmentPathHelpers
-      def add_env_path(path)
 
+      def configure_ruby_env_paths(user_dir)
+        user_profile = File.join(user_dir, node['rvm']['user_env']['profile_filename'])
+        file user_profile do
+            action :touch
+        end
+
+        node['rvm']['user_env']['paths'].each do |path|
+          bin = File.join(user_dir, path)
+          fe = Chef::Util::FileEdit.new(user_profile)
+          fe.insert_line_if_no_match(/#{bin}/, "export PATH=#{bin}:$PATH")
+        end
+        fe.write_file
       end
+
+    end
+  end
 end
